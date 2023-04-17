a55b77882d89fe6e199217ee28745d9f219dc8d0
fndlwl64 <fndlwl64@gmail.com>
2023-04-17 16:10:09 +0900

    Initial commit

---
+++ src/main/java/com/springboot/sns_community/SnsCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.sns_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/sns_community/SnsCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.sns_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
